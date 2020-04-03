cbd497f782f4cc7a2532ce3b22cdf25b1b5a8f48
claire-labry <clairestjlabry@gmail.com>
2020-04-03 17:43:33 -0500

    added spotify-this-song and movie-this to liri.js

--- liri.js
+++ liri.js
@@ -48,10 +48,12 @@ switch(commandLiri) {
     console.log("Not recgonized command. Please these commands only for the liri app: concert-this, spotify-this-song, movie-this, do-what-it-says");
 };
 
-function concertThis(value) {
-    axios.get("https://rest.bandsintown.com/artists/"+ artist + "/events?app_id=codingbootcamp").then(
+// function pulls the artist name from the Bands in Town API
+function concertThis(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
         function(response){
-
+    
+            // for loop that collects the data of the user's request
             for(var i = 0; i < response.data.length; i++){
                 
                 var datetime = response.data[i].datetime;
@@ -59,7 +61,7 @@ function concertThis(value) {
 
                 var concertChoice = 
 
-                "-------------Concert Choice-------------" +
+                "-------------Concert Information-------------" +
                 "\n Venue Name: " + response.data[i].venue.name +
                 "\n City: " + response.data[i].venue.city +
                 "\n Country: " + response.data[i].venue.country +
@@ -68,5 +70,64 @@ function concertThis(value) {
                 console.log(concertChoice);
 
             } 
+        })
+        .catch(function (err) {
+            console.log(err);
+        });         
+    }
+    
+    function spotifyThis(value){
+         if(!value){
+             value = "The Sign"; 
+         }
+         spotify
+         .search({
+             type: "track",
+             query: value
+         })
+        .then(function(response){
+            for(var i = 0; i < 3; i++){
+            
+                var spotifyChoice =
+
+                "-------------Spotify Choice-------------"
+
+                "\n Artist: " + response.tracks.items[i].artists.name +
+                "\n Song Name: " + response.tracks.items[i].name +
+                "\n Preview Link: " + response.tracks.items[i].preview_url +
+                "\n Album Name: " + response.tracks.items[i].album.name;
+
+                console.log(spotifyChoice);
+            }
+        })
+    
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
+
+    function movieThis(movie){
+        axios.get("http://www.omdbapi.com/?t="+ movie + "&y=&plot=short&apikey=trilogy").then(
+            function(response){
+        
+                var movieChoice = 
+
+                "-------------Movie Information-------------"
+
+                "\n Movie Title: " + response.data.Title +
+                "\n Year Released: " + response.data.Year +
+                "\n IMDB Rating: " + response.data.imdbRating +
+                "\n Rotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                "\n Country Produced: " + response.data.Country +
+                "\n Movie Language: " + response.data.Language +
+                "\n Movie Plot: " + response.data.Plot +
+                "\n Movie's Actors/Actresses: " + response.data.Actors +
+
+                console.log(movieChoice);
+    
+                
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
         }
-    )}; // End Syntax for concert-this
